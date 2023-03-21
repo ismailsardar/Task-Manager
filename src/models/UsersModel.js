300f0a50b13877a5e73a681b1c0d469e7a099b7c
ismail <ismailsardar@gmail.com>
2023-03-21 19:48:25 +0600

    user model created

---
+++ src/models/UsersModel.js
@@ -0,0 +1,17 @@
+/**
+ * task-menage projects / user models
+ * Date : 21/03/2023
+ * auth: Ismile Sardar
+ */
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    email:{type:String,unique:true},
+    firstName:{type:String},
+    lastName:{type:String},
+    mobile:{type:String},
+    password:{type:String},
+    photo:{type:String},
+},{versionKey:false,timestamps:true});
+
+const UsersModel = mongoose.model('users',userSchema);
+module.exports = UsersModel;
