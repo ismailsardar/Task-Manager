108861b9a8adb2c1989baf7746ffec2abc92a9ca
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-26 16:24:32 +0600

    mongoodb conections

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
-    console.log(`app runing at ${PORT}`);
+    console.log(`app running at ${PORT}`);
 })
