7ce6ed0814f399d9a7cc3074e16f92b19c1aa29b
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-03 19:39:36 +0600

    smtp server html update

--- src/utility/SMTPEmail.js
+++ src/utility/SMTPEmail.js
@@ -21,8 +21,8 @@ const SendEmailUtility = async (EmailTo, EmailText, EmailSubject) => {
     // text: EmailText,
     html: `<html>
     <body style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5; color: #333;">
-      <h1 style="font-size: 24px; margin-bottom: 20px; text-align: center;">${EmailText}</h1>
-      <p style="margin-bottom: 10px;"></p>      
+      <h1 style="font-size: 32px; margin-bottom: 20px; text-align: center; color: blue">${EmailText}</h1>
+      <p style="margin: 10px; font-size: 17px; text-align: center;">This is your OTP code. It's expire with 2 manet</p>      
     </body>
   </html>`,
   };
